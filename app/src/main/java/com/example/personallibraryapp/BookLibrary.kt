b442607c5c7c87eccbf235e54ae34491c1084646
kamrul <kamrulhasankganj@gmail.com>
2024-11-04 10:37:06 +0600

    Initial commit

---
+++ app/src/main/java/com/example/personallibraryapp/BookLibrary.kt
@@ -0,0 +1,15 @@
+package com.example.personallibraryapp
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity(tableName = "books")
+data class BookLibrary(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val title: String,
+    val author: String,
+    val pages: String,
+    var isRead: Boolean = false
+): Serializable
